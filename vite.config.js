a4214eb06ab1a693cc28c85c3c3fd39a093bc8d5
You Know <111015172+OptimisticImpulse@users.noreply.github.com>
2024-04-18 11:12:41 -0400

    Update vite.config.js
    Signed-off-by: You Know <111015172+OptimisticImpulse@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
   plugins: [react()],
-  build: {
-    outDir: '..',
-    emptyOutDir: true,
-    cssMinify: false,
-  },
-});
+  base: "/scaling-broccoli/"
+})
